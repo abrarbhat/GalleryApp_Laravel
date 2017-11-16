4cf551780d9f5522cf77311e78d9fb117fc58475
John Appleseed <email@example.com>
2017-11-16 23:56:51 +0530

    after incluinding admin layout

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,9 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('/admin','AdminUsersController');
+
+Route::get('/g',function (){
+
+    return view('admin.index');
+
+});
